fa036cf5111dab38e7500de144e3528afe7fdf4a
Le Duc Long <longld@mz.co.kr>
2024-06-06 17:21:43 +0700

    feat: finishing expense list page
    - Add new skeleton
    - Add table and map data value

--- client/src/routes/create-expense.tsx
+++ client/src/routes/create-expense.tsx
@@ -1,5 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/create-expense')({
-  component: () => <div>Hello /create-expense!</div>
-})
+export const Route = createFileRoute("/create-expense")({
+  component: CreateExpense,
+});
+
+function CreateExpense() {
+  return (
+    <div className="p-2">
+      <h1>Create Expense</h1>
+      <form className=" flex flex-col border border-white rounded-sm p-5 w-fit gap-5">
+        <div className="flex justify-between gap-2">
+          <label>Name:</label>
+          <Input type="text" />
+        </div>
+        <div className="flex justify-between gap-2">
+          <label>Amount:</label>
+          <Input type="number" />
+        </div>
+        <Button className="w-fit m-auto" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
--- client/src/routes/expenses.tsx
+++ client/src/routes/expenses.tsx
@@ -1,5 +1,76 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/expenses')({
-  component: () => <div>Hello /expenses!</div>
-})
+export const Route = createFileRoute("/expenses")({
+  component: Expenses,
+});
+
+const getAllExpenses = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const res = await api.expenses.$get();
+  if (!res.ok) {
+    throw new Error("Failed to get expenses");
+  }
+  const data = await res.json();
+  return data;
+};
+
+function Expenses() {
+  const { data, error, isPending } = useQuery({
+    queryKey: ["get-all-expenses"],
+    queryFn: getAllExpenses,
+  });
+
+  if (error) return <div>An error has occurred: {error.message}</div>;
+
+  return (
+    <div className="p-2 max-w-3xl m-auto">
+      <Table>
+        <TableCaption>A list of your expenses</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+            ? Array(3)
+                .fill(0)
+                .map((_, i) => (
+                  <TableRow key={i}>
+                    <TableCell className="font-medium animate-pulse">
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell className="animate-pulse">
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell className="animate-pulse">
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                  </TableRow>
+                ))
+            : data.map((expense) => (
+                <TableRow key={expense.id}>
+                  <TableCell className="font-medium">{expense.id}</TableCell>
+                  <TableCell>{expense.title}</TableCell>
+                  <TableCell>{expense.amount}</TableCell>
+                </TableRow>
+              ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
